76df0f97d65a51bb556aeadf5bc411232fd609a4
deeppak_1 <115033744+Vinod123456183@users.noreply.github.com>
2025-01-13 23:32:37 +0530

    Create main.js

---
+++ Shreiyans-Backend/Video-1/main.js
@@ -0,0 +1,23 @@
+// const arr = [1 , 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// arr.forEach(function(item){
+//     console.log(item+'as');
+// })
+
+// const arr = [5, 6, 7, 8, 9, 10];
+// const newSpacearr = arr.map(function(item){
+//     return 1;
+// })
+// console.log(newSpacearr);
+
+// const arr = [1,2,3];
+// const newAr = arr.filter(function(item){
+//     return item > 2;
+// })
+
+// console.log(newAr);
+
+// const arr = [1,2,3,3,4];
+// const newa = arr.find(function(item){
+//     return item  === 2;
+// })
+// console.log(newa);
