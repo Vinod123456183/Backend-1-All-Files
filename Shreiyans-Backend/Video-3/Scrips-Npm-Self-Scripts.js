b91cbf8f6958c263f5c20796f7dd4135bb5705e9
deeppak_1 <115033744+Vinod123456183@users.noreply.github.com>
2025-01-14 17:25:28 +0530

    Create Scrips-Npm-Self-Scripts.js

---
+++ Shreiyans-Backend/Video-3/Scrips-Npm-Self-Scripts.js
@@ -0,0 +1,17 @@
+when we install node and npm the os already make a take 
+start and test 
+so if we write command like npm start so if find packagae therer 
+
+
+npm chacha wont work like it 
+
+to make it run we have to change in package.json and then 
+npm run chacha
+
+Like 
+"dev": " node --env-file=.env --watch main.js"
+"start" : "node main.js"
+"chacha" : "echo "\Hello\" "
+
+
+custom scripts in concurency server like 2 server ek sath react and node server in mern
