c814614ff98065f95f734334b405fc092d47c4a1
deeppak_1 <115033744+Vinod123456183@users.noreply.github.com>
2025-01-17 23:56:36 +0530

    Create main.js

---
+++ Shreiyans-Backend/Video 10 MongoDB Basics/main.js
@@ -0,0 +1,21 @@
+// data storeafe where we can store information of persons
+// database can store everythnign and more efficient than others 
+
+// tyoes
+//  sql , no sql
+// sql me data doesnt stire in row cool just we do to display data so user can understand it properly
+
+// no sql me data store in json format js , key value pair
+
+// Backend contains two server 
+// application { node , } and database server {mongo , db}
+
+// when we request then it goes to application server , it do all thing 
+
+// DATABASE -> COLLECTION -> DOCUMENT
+
+// CODE :                     DATABASE
+
+// moongose.connetct   ->       database create
+// model create            ->      collection create
+// create code              ->      document create
