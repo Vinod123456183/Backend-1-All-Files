ec568fb8733cbc6394dc4d2d43fc29b54c5fc1a5
deeppak_1 <115033744+Vinod123456183@users.noreply.github.com>
2025-01-28 15:27:17 +0530

    Create Methods.js

---
+++ Backend-Domination-Shreiyans/5 Different-Post-Method-And-Error-Handliing/Methods.js
@@ -0,0 +1,11 @@
+// GET - server se kuch lana
+// kuch keh dena server ko ki esa kr do ya wo kr do
+
+// post - server pr kuch change hoga , is aspect me ki change hoga
+// ek matki rkh le , kuch change hua 
+
+// put -update and  pura replace kr deta h,
+// aajse name harshit 
+
+// patch - update and  pura replace nahi krta  h, y sirf wahi jo change hona h
+// like name change toh name hi change hoga
