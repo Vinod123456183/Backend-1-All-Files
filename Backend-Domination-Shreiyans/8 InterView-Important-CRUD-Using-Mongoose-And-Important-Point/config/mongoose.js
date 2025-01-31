660090a24e5764b0139c168800f9dbfbe807ae8a
deeppak_1 <115033744+Vinod123456183@users.noreply.github.com>
2025-01-31 20:54:55 +0530

    Create mongoose.js

---
+++ Backend-Domination-Shreiyans/8 InterView-Important-CRUD-Using-Mongoose-And-Important-Point/config/mongoose.js
@@ -0,0 +1,21 @@
+  const mongoose = require('mongoose');
+  // if database created then it connect else mongodb itself create
+  // not industry standards
+  const debuglog = require('debug')('development:mongooseconfig')
+  
+  mongoose.connect('mongodb://127.0.0.1:27017/testingdb');    // Connect to MongoDB with mongoose (without deprecated options)
+  const db = mongoose.connection; // Get the connection object
+  
+  db.on("error", (err) => {   // Log error
+      debuglog("Error: ", err);
+  });
+  
+  db.on("open", () => {
+      debuglog("Connected to Database!");
+  });
+  
+  module.exports = db;
+  
+  
+  // Type npm i debug 
+  // $env:DEBUG='development:mongooseconfig'
