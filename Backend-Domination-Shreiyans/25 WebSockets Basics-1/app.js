2dcce10302eca19f8c415687422531a83aa89bb6
deeppak_1 <115033744+Vinod123456183@users.noreply.github.com>
2025-03-09 08:46:15 +0530

    Add files via upload

--- Backend-Domination-Shreiyans/25 WebSockets Basics-1/app.js
+++ Backend-Domination-Shreiyans/25 WebSockets Basics-1/app.js
@@ -1,1 +1,24 @@
-a
+const express = require("express");
+const app = express();
+const socketIo = require("socket.io");
+const http = require("http");
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.set("view engine", "ejs");
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+});
+
+app.get("/", (req, res) => {
+  // humne y page khola
+  //   fronted pr chla y io function
+  //   iski wajah se frontend se backend pr gya connection event
+  //   and backend pr handle kiya h
+  res.render("index");
+});
+
+server.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
