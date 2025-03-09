edac6e2d5174ff3ee655d0494436c9c578ebc92d
deeppak_1 <115033744+Vinod123456183@users.noreply.github.com>
2025-03-09 08:44:56 +0530

    Create userModel.js

---
+++ Backend-Domination-Shreiyans/24 Caching Basics/model/userModel.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+});
+const user = mongoose.model("user", userSchema);
+module.exports = user;
