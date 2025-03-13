32fb9266a554ed1ac4c66ac77ef06b9f1edd2edb
deeppak_1 <115033744+Vinod123456183@users.noreply.github.com>
2025-03-13 11:09:07 +0530

    Add files via upload

---
+++ Backend-Domination-Shreiyans/32 Payment GateWay Integration/app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const indexRouter = require("./routes/index");
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/", indexRouter);
+
+app.listen(3000);
