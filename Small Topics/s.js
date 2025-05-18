d36e59eaf8905b849acb2032f5b2d1238fe2ceaa
deeppak_1 <115033744+Vinod123456183@users.noreply.github.com>
2025-05-18 10:33:28 +0530

    Create s.js

---
+++ Small Topics/s.js
@@ -0,0 +1,30 @@
+// async js
+// fetching data from net ,fetching data from the internet, reading files, 
+// or performing heavy calculations can be done in the background, allowing the rest of the program to keep running.
+// Async in JavaScript allows your program to do things in the background without making the whole program wait. It helps in handling tasks like waiting for data or a response without freezing everything else.
+
+// callback , promises, async await
+
+// resolve h toh then chlega 
+
+// cjs , esm
+// cjs me require hoga  , cjs me module.exports hoga 
+// esm me import hoga , esm me export hoga
+
+let a = 10 ;
+let b = 20 ;
+// exports ek hi chiij export kr skta h
+module.exports = {a , b};
+
+
+// jbhi hum device me internet on krte h tbhi ip address generate hota h
+// ip address isp deta h
+// have 2 types of ip address ipv4 and ipv6
+// it is given by dhcp protocol
+
+// mac address is insode phone , laptop , computer
+// insmall area network , mac address is used
+// mac address is unique for every device
+// ip address for big network
+
+// udp jldise se jaldi data bhejta h
